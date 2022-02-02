9d8542b0a38c6e106c308dc7b5004277e00d7712
Mohammad-Shdooh <Mohammad.shdooh.msh@gmail.com>
2022-02-02 14:12:34 +0200

    Database

--- E-shop/Admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once "DbConn.php";
-include_once "sidebar.php";
-include_once "Admin_item.php";
-
-?>
--- E-shop/home.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include_once "navbar.php";
-include_once "item.php";
-
-
-
-?>
